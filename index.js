e37e0d7ceef6418f929c6d21e2f53c066a279715
Hannes <hannes.tamm@tptlive.ee>
2026-08-15 17:11:42 +0300

    Add second generated password option

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordLength = 12
+let passwordLengthOne = 8
+let passwordLengthTwo = 12
 
-let passwordGenerated = document.getElementById("generated-password");
+let passwordGeneratedOne = document.getElementById("generated-password-one");
+let passwordGeneratedTwo = document.getElementById("generated-password-two");
 
 function getRandomChar(){
     let randomChar = Math.floor(Math.random() * characters.length);
@@ -11,9 +13,14 @@ function getRandomChar(){
 }
 
 function generateRandomPassword(){
-    let randomPassword = ''
-    for(let i = 0; i < passwordLength; i++){
-        randomPassword += getRandomChar();
+    let randomPasswordOne = '';
+    let randomPasswordTwo = '';
+    for(let i = 0; i < passwordLengthOne; i++){
+        randomPasswordOne += getRandomChar();
     }
-    passwordGenerated.textContent = randomPassword
+    for(let i = 0; i < passwordLengthTwo; i++){
+        randomPasswordTwo += getRandomChar();
+    }
+    passwordGeneratedOne.textContent = randomPasswordOne;
+    passwordGeneratedTwo.textContent = randomPasswordTwo;
 }
